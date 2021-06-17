9caf6059d299c87cc6c3ad95027ffb3a5c306da9
Tran Trung Kien <tieulong2803200@gmail.com>
2021-06-17 10:58:45 +0700

    [Thực hành] Ứng dụng quản lý danh sách khách hàng

---
+++ customer-management-orm/src/main/java/cg/wbd/grandemonstration/service/impl/HibernateCustomerServiceImpl.java
@@ -0,0 +1,61 @@
+package cg.wbd.grandemonstration.service.impl;
+
+import cg.wbd.grandemonstration.model.Customer;
+import cg.wbd.grandemonstration.service.CustomerService;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
+@Service
+public class HibernateCustomerServiceImpl implements CustomerService {
+    @Override
+    public List<Customer> findAll() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public Customer findOne(Long id) {
+        return null;
+    }
+
+    @Override
+    public Customer save(Customer customer) {
+        return null;
+    }
+
+    @Override
+    public List<Customer> save(List<Customer> customers) {
+        return null;
+    }
+
+    @Override
+    public boolean exists(Long id) {
+        return false;
+    }
+
+    @Override
+    public List<Customer> findAll(List<Long> ids) {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public long count() {
+        return 0;
+    }
+
+    @Override
+    public void delete(Long id) {
+    }
+
+    @Override
+    public void delete(Customer customer) {
+    }
+
+    @Override
+    public void delete(List<Customer> customers) {
+    }
+
+    @Override
+    public void deleteAll() {
+    }
+}
